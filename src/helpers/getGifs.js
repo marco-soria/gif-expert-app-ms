11dc1db368d4fc12bf3ee4cb8282247658b37f47
marco-soria <marco.soria@pucp.edu.pe>
2024-04-15 19:56:30 -0500

    env variables

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async (category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=psWq55EdXLGRzN1Xb97hq6YSDbgTzSHm&q=${category}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_APP_GIPHY_API_KEY}&q=${category}&limit=10`
     const response = await fetch(url);
     const { data = [] } = await response.json();
 
